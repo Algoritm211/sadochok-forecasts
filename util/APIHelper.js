e0c1b8e8d7d85d727d9b71efb780d2b82b765c76
DfyzET <freeman.crazys@gmail.com>
2021-12-04 17:10:40 +0200

    Create util for getting data from betting API. For all questions - DM Tim

---
+++ util/APIHelper.js
@@ -0,0 +1,53 @@
+const path = require('path');
+const {DateTime} = require('luxon')
+require('dotenv').config({
+  path: path.resolve(__dirname, '../.env')
+});
+const axios = require('axios');
+const api_token = process.env.REACT_APP_API_KEY;
+const sport = 'soccer';
+const regions = 'eu';
+const markets = 'h2h';
+
+
+// Main Get Func From API
+const GetAllMatches = async (sportType) => {
+  try {
+    const {data} = await axios.get(`https://api.the-odds-api.com/v4/sports/${sportType}/odds/?apiKey=${api_token}&regions=${regions}&markets=${markets}`);
+    return FormatDataForBot(data);
+  } catch (error) {
+    console.log('Error status', error);
+  }
+}
+
+//Format Data Before Goes To Bot
+function FormatDataForBot(arrayData) {
+  let formatData = arrayData;
+  formatData.forEach(FormatObject);
+  return formatData;
+}
+
+function FormatObject(item, index) {
+  let homePrice = 1;
+  let awayPrice = 1;
+  let prices = item['bookmakers'][0]['markets'][0]['outcomes'];
+  for (let i = 0; i < Object.keys(prices).length; i++) {
+    if (prices[i]['name'] == item['home_team']) {
+      homePrice = prices[i]['price'];
+    } else if (prices[i]['name'] == item['away_team']) {
+      awayPrice = prices[i]['price'];
+    }
+  }
+  item['sport_versus'] = item['home_team'] + ' vs ' + item['away_team'];
+  const date = DateTime.fromJSDate(new Date()).toFormat('dd/mm/yyyy');
+  const time = DateTime.fromJSDate(new Date()).toFormat('HH:mm');
+  item['date'] = date;
+  item['time'] = time;
+  item['home_price'] = homePrice;
+  item['away_price'] = awayPrice;
+  delete item['bookmakers'];
+  delete item['commence_time'];
+}
+
+//For Enter Just Use:
+//GetAllMatches(sport).then((data) => console.log(data));
